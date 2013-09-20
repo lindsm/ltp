a8ba155c3309ba21df04bcf2739c92699f5c4109
Mike Lindsay <lindsm@gmail.com>
2013-09-19 17:47:48 -0700

    First initialization of LTP class repository

---
+++ week3/exercise3.py
@@ -0,0 +1,29 @@
+# dollars = 8
+# print 8 >= dollars
+# print dollars == 8.0
+
+# instructors = 2
+# print not instructors < 1
+
+# dollars = 18
+# cents = 53
+
+# print not (dollars < cents and dollars > 0)
+# print dollars == 10 or not cents != 53
+# print not dollars < 10 and cents > 15
+# print not dollars == 18 or not cents == 53
+# eggs = 12
+
+# print eggs % 12 != 0
+
+# age1 = input("How old are you? ")
+# age2 = input("How old is your best friend? ")
+
+# print(int(age1) + int(age2))
+
+# x = int(age1)
+# y = int(age2)
+# print(str(x + y))
+
+import math
+print(math.factorial(8))
